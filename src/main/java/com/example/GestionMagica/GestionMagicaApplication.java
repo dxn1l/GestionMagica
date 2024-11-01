968c98978f65f40d2f4ab2222c1001c59ba4833f
dxn1l <danielmorenorey2012@gmail.com>
2024-11-01 15:44:20 +0100

    Initial commit

---
+++ src/main/java/com/example/GestionMagica/GestionMagicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionMagica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMagicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMagicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GestionMagica/GestionMagicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionMagica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMagicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
