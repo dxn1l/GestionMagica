3521a7ed066bc7aeaba921e6da968fbe79927f95
Arturo <artuloca@myuax.co>
2024-11-02 17:54:23 +0100

    puerto

--- src/main/java/com/example/GestionMagica/User.java
+++ src/main/java/com/example/GestionMagica/User.java
@@ -1,7 +1,7 @@
 package com.example.GestionMagica;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
